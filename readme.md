9d2987ded30e04c63942fb55abf45ebc8d89443c
Monica Wheeler <monica.c.wheeler@gmail.com>
2017-12-18 13:41:17 -0600

    Update bkgd color to change yellow on first click and back to red on second click

--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@
 - [X] Append a div element to DOM when Generate is clicked
 - [ ] TODO - Add the number of the clicks in each sequential div
 - [X] Add swap and delete buttons
-- [ ] Style div created with generate button, bkgd red
-- [ ] Update bkgd color to change yellow on first click and back to red on second click (toggle)
+- [X] Style div created with generate button, bkgd red
+- [X] Update bkgd color to change yellow on first click and back to red on second click (toggle)
 - [ ] Delete button deletes the enter div created
--- scripts.js
+++ scripts.js
@@ -5,14 +5,21 @@ $(document).ready(divManager);
 function divManager() {
   // Event Listeners
   $('#generate-btn').on('click', divCreator);
+  $('#content-container').on('click', '.swap-button', colorSwapper);
 
   function divCreator () {
     // Append div to DOM when generate is clicked
     $('#content-container').append('<div class="block"></div>');
-    // Create <p>
+    // Create <p> tag
+    var count = 0;
     $('.block').html('<p>' + count + '</p>');
     // Create buttons for swap and delete
     $('.block').html('<button class="swap-button">Swap</button><button class="delete-button">Delete</button>');
+  }
+
+  function colorSwapper() {
+    // Swap color with toggleClass on button click
+    $(this).parents('.block').toggleClass('yellow');
   }
 
 }
